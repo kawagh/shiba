fd30caf0e95f379b0f8cf353a92a7feb9e8281e9
kawagh <kawagh@example.com>
2022-03-13 10:05:46 +0900

    [ui] displayed 7 elements list

--- app/src/main/java/com/example/shiba/MainActivity.kt
+++ app/src/main/java/com/example/shiba/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.shiba
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.shiba.ui.theme.Glass
 import com.example.shiba.ui.theme.ShibaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
@@ -30,14 +37,63 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "main") {
+        composable("main") { MainScreen() }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ShibaTheme {
-        Greeting("Android")
+fun MainScreen() {
+    val progresses = List(7) { it % 2 == 0 }.toMutableStateList()
+    val onPanelClick: (Int) -> Unit = {
+        progresses[it] = !progresses[it]
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = "recent 7 days",
+            fontSize = 20.sp
+        )
+        PanelRow(panels = progresses, onPanelClick = onPanelClick)
+    }
+}
+
+@Composable
+fun PanelRow(
+    panels: SnapshotStateList<Boolean>,
+    onPanelClick: (Int) -> Unit,
+) {
+    Row {
+        panels.forEachIndexed { index, panel ->
+            Panel(
+                panel,
+                onClick = { onPanelClick(index) }
+            )
+        }
+    }
+}
+
+@Composable
+fun Panel(panel: Boolean, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .size(30.dp),
+        colors = when (panel) {
+            true ->
+                ButtonDefaults.textButtonColors(
+                    backgroundColor = Glass
+                )
+            false ->
+                ButtonDefaults.textButtonColors(
+                    backgroundColor = Color.White
+                )
+        }
+    ) {
     }
 }
