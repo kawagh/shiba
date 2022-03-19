136d33f94123cc4728d011ebafa783328fd8c776
kawagh <kawagh@example.com>
2022-03-20 08:59:53 +0900

    [feat] implemented showing Toast #19

--- app/src/main/java/com/example/shiba/RegisterContent.kt
+++ app/src/main/java/com/example/shiba/RegisterContent.kt
@@ -1,5 +1,6 @@
 package com.example.shiba
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,9 +11,11 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun RegisterContent(handleAddClick: (String) -> Unit) {
+    val context = LocalContext.current
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -31,6 +34,7 @@ fun RegisterContent(handleAddClick: (String) -> Unit) {
             Button(onClick = {
                 if (text.isNotEmpty()) {
                     handleAddClick(text)
+                    Toast.makeText(context, "`$text` Registered", Toast.LENGTH_SHORT).show()
                 }
             }) {
                 Text(text = "Add")
